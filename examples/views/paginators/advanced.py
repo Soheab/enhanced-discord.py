0c0ddde825bc718d60768a20a4ea05f5a9b01f18
Soheab <soheabdebeste@hotmail.com>
2021-12-21 20:14:03 +0100

    some things

--- examples/views/paginators/advanced.py
+++ examples/views/paginators/advanced.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 from discord.ui import view
 
 MISSING = discord.utils.MISSING
+P = TypeVar("P", bound="Paginator")
 
 
 class PaginatorButton(discord.ui.Button["Paginator"]):
@@ -60,9 +61,9 @@ class PaginatorButton(discord.ui.Button["Paginator"]):
 
         # elif self.view.current_page >= self.view.max_pages - 1:
 
-        keywords, _ = await self.view.handle_page_content()
+        page_kwargs = await self.view.get_page_kwargs(self.view.current_page)
         assert interaction.message is not None
-        await interaction.message.edit(**keywords)
+        await interaction.message.edit(**page_kwargs)
 
 
 class Paginator(discord.ui.View):
@@ -75,8 +76,8 @@ class Paginator(discord.ui.View):
     def __init__(
         self,
         pages: Union[List[discord.Embed], List[str]],
+        ctx: Optional[commands.Context] = MISSING,
         *,
-        author_id: int = None,
         disable_on_timeout: bool = False,
         delete_message_after: bool = False,
         clear_after: bool = False,
@@ -96,7 +97,7 @@ class Paginator(discord.ui.View):
 
         # assign the basic attributes
 
-        self._author_id = author_id
+        self.ctx: Optional[commands.Context] = ctx
         self._disable_on_timeout = disable_on_timeout
         self._delete_message_after = delete_message_after
         self._clear_after = clear_after
@@ -111,15 +112,18 @@ class Paginator(discord.ui.View):
         self.page_string = f"Page {self.current_page}/{self.max_pages}"
 
         # adds the buttons to the view
-        self.add_buttons()
+        self._add_buttons()
 
-    def add_buttons(self):
+    def _add_buttons(self):
         if all(b in ["first", "left", "right", "last", "stop"] for b in self.buttons.keys()) is False:
-            raise ValueError("Paginator buttons must have a left, right and stop button")
+            raise ValueError("Buttons keys must be one of 'first', 'left', 'right', 'last', 'stop'")
+
+        if all(isinstance(b, PaginatorButton) for b in self.buttons.values()) is False:
+            raise ValueError("Buttons values must be PaginatorButton instances")
 
         # no need to run a view and add buttons if there are no pages.
         if self.max_pages <= 1:
-            self.stop()
+            super().stop()
             return
 
         button: PaginatorButton
@@ -146,9 +150,9 @@ class Paginator(discord.ui.View):
 
             self.add_item(button)
 
-        self.__set_position()
+        self._set_button_positions()
 
-    def __set_position(self) -> None:
+    def _set_button_positions(self) -> None:
         """Moves the buttons to the desired position"""
         button: PaginatorButton
         for button in self.children:  # type: ignore
@@ -158,78 +162,57 @@ class Paginator(discord.ui.View):
     async def format_page(self, page: Union[discord.Embed, str]) -> Union[discord.Embed, str]:
         return page
 
-    async def handle_page_content(
-        self, kwargs: Optional[Any] = None
-    ) -> Union[Tuple[Dict[str, Any], None], Tuple[Dict[str, Any], Dict[str, Any]], Tuple[dict, None]]:
-        self.page_string: str = f"Page {self.current_page + 1}/{self.max_pages}"  # type: ignore
+    async def get_page_kwargs(
+        self: P, page: int, send_kwargs: Optional[Dict[str, Any]] = None
+    ) -> Tuple[Dict[Literal["content", "embed", "view"], Union[discord.Embed, str, P, None]], Dict[str, Any]]:
 
-        # get page content aka see if it's an embed or string.
-        keywords = await self.get_page_kwargs(self.current_page)
-
-        # check if keywords is None aka page is over the max pages
-        if keywords is None:
-            return {}, kwargs
-
-        if kwargs is not None:
+        if send_kwargs is not None:
             # remove any content and embeds provided via send because that will conflict with the page content
             try:
-                del kwargs["content"]
-                del kwargs["embed"]
-                del kwargs["embeds"]
+                del send_kwargs["content"]
+                del send_kwargs["embed"]
+                del send_kwargs["embeds"]
             except KeyError:
                 pass
 
+        self.page_string: str = f"Page {self.current_page + 1}/{self.max_pages}"  # type: ignore
+
+        formatted_page: Union[str, discord.Embed, None] = await discord.utils.maybe_coroutine(self.format_page, self.pages[page])  # type: ignore
+
         # check if page is a string
-        if keywords["content"] is not None:
-            # add the page string (current_page/max_pages) as content after the page content
-            keywords["content"] += f"\n\n{self.page_string}"
-
-        # check if page is an embed
-        elif keywords["embed"] is not None:
-            # add the page string (current_page/max_pages) as the footer of the embed
-            keywords["embed"].set_footer(text=self.page_string)
-
-        return keywords, kwargs
-
-    async def get_page_kwargs(self, page_number: int) -> Optional[Dict[str, Any]]:
-
-        # check if page is over the max pages and return None
-        if page_number < 0 or page_number >= self.max_pages:
-            return None
-
-        # get the page
-        page: Coroutine[Any, Any, Union[str, discord.Embed]] = await discord.utils.maybe_coroutine(
-            self.format_page, self.pages[page_number]
-        )
-        # check if page is a string
-        if isinstance(page, str):
-            return {"content": page, "embed": None, "view": self}
+        if isinstance(formatted_page, str):
+            formatted_page += f"\n\n{self.page_string}"
+            return {"content": formatted_page, "embed": None, "view": self}, send_kwargs or {}
 
         # check if the page is an embed
-        elif isinstance(page, discord.Embed):
-            return {"content": None, "embed": page, "view": self}
+        elif isinstance(formatted_page, discord.Embed):
+            formatted_page.set_footer(text=self.page_string)
+            return {"content": None, "embed": formatted_page, "view": self}, send_kwargs or {}
 
         # if the page is neither a string or an embed, raise an error
         else:
-            raise TypeError(f"{page.__class__} is not a string or an embed.")
+            return {}, send_kwargs or {}
 
     async def on_timeout(self) -> None:
-        await self._stop_menu()
+        await self.stop()
 
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        assert interaction.user is not None  # so that the type checker doesn't complain
-
-        # always return True when we don't have the author_id set
-        if not self._author_id:
+    async def interaction_check(self, interaction: discord.Interaction):
+        if not interaction.user or not self.ctx:
             return True
 
-        # if the user is the author of the message, allow the interaction
-        return interaction.user.id == self._author_id
+        if not interaction.user.id in {
+            getattr(self.ctx.bot, "owner_id", None),
+            self.ctx.author.id,
+            *getattr(self.ctx.bot, "owner_ids", {}),
+        }:
+            return False
 
-    async def _stop_menu(self):
+        return True
+
+    async def stop(self):
+        super().stop()
+
         assert self.message is not None  # so that the type checker doesn't complain
-
-        self.stop()
 
         if self._delete_message_after:
             await self.message.delete()
@@ -251,13 +234,18 @@ class Paginator(discord.ui.View):
         else:
             return
 
+    async def send_with_interaction(self, interaction: discord.Interaction, *args, **kwargs) -> None:
+        page_kwargs, send_kwargs = await self.get_page_kwargs(self.current_page, kwargs)
+
+        await interaction.response.send_message(*args, **page_kwargs, **send_kwargs)
+
     # main way to send the menu
     async def send(
         self, send_to: Union[discord.abc.Messageable, discord.Message], *args: Any, **kwargs: Any
     ) -> discord.Message:
 
         # get the page content
-        keywords, kwargs = await self.handle_page_content(kwargs)  # type: ignore
+        page_kwargs, send_kwargs = await self.get_page_kwargs(self.current_page, kwargs)
 
         # raise if send_to is None
         if not send_to:
@@ -266,10 +254,10 @@ class Paginator(discord.ui.View):
         # check if send_to is a message or channel. If it is a message we reply to it else we send it to the channel
         if isinstance(send_to, discord.Message):
             # send_to is a message, so we reply to it
-            self.message = await send_to.reply(*args, **keywords, **kwargs)  # type: ignore
+            self.message = await send_to.reply(*args, **page_kwargs, **send_kwargs)  # type: ignore
         else:
             # send_to is a channel, so we send it
-            self.message = await send_to.send(*args, **keywords, **kwargs)  # type: ignore
+            self.message = await send_to.send(*args, **page_kwargs, **send_kwargs)  # type: ignore
 
         # return the sent message
         assert self.message is not None  # so that the type checker doesn't complain
